b3ae63af9b7f04986d6536e60d5e47270d2e7ab0
Jarno <jarno_lahti@icloud.com>
2021-03-18 18:18:13 +0200

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# COFFEE BREAK CODING: aws s3 nodejs streams download and upload
+
+This was a study mainly on how to use streams to upload and download files from AWS S3.
+
+## How to run
+
+* Clone this project
+* Run `yarn install`
+* Create `.env` file inside root of the project
+* Add `AWS_S3_BUCKET_NAME=<your bucket name>` to the `.env` file
+* OPTIONAL: Add `PORT=<your port>` to the `.env` file. Default is `PORT=3000`
+* Run `yarn start`
+* Open your browser and go to `http://locahost:<your port>`
+
--- index.js
+++ index.js
@@ -5,23 +5,25 @@ import Busboy from 'busboy';
 import { ManagedDownloader } from '@aws/s3-managed-downloader';
 import * as AWS from 'aws-sdk';
 
-if (!process.env.AWS_BUCKET_NAME) {
-  throw Error("NO 'AWS_BUCKET_NAME' DEFINED IN ENVIRONMENT: define it in .env file which should be found from root of this project");
+const port = process.env.PORT || 3000;
+
+if (!process.env.AWS_S3_BUCKET_NAME) {
+  throw Error("NO 'AWS_S3_BUCKET_NAME' DEFINED IN ENVIRONMENT: define it in .env file which should be found from root of this project");
 }
 
 const s3 = new AWS.S3({ region: 'eu-central-1', apiVersion: '2006-03-01' });
 s3.config.logger = console;
 const downloader = new ManagedDownloader(s3);
 
-const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 
 const s3Upload = async (fileStream, fileName) => {
-  await s3.upload({ Bucket: AWS_BUCKET_NAME, Key: fileName, Body: fileStream }, { partSize: 10 * 1024 * 1024 }).promise();
+  await s3.upload({ Bucket: AWS_S3_BUCKET_NAME, Key: fileName, Body: fileStream }, { partSize: 10 * 1024 * 1024 }).promise();
 };
 
 const s3Download = async (key) => {
   return await downloader.getObjectStream({
-    Bucket: AWS_BUCKET_NAME,
+    Bucket: AWS_S3_BUCKET_NAME,
     Key: key,
   });
 };
@@ -36,7 +38,7 @@ const handlePosts = (req, res) => {
 };
 
 const handleGetObjects = async (res) => {
-  const getResult = await s3.listObjects({ Bucket: AWS_BUCKET_NAME, MaxKeys: 10 }).promise();
+  const getResult = await s3.listObjects({ Bucket: AWS_S3_BUCKET_NAME, MaxKeys: 10 }).promise();
   res.end(JSON.stringify(getResult.Contents.map((c) => c.Key)));
 };
 
@@ -49,7 +51,7 @@ const handleDownload = async (id, res) => {
 };
 
 const handleDeleteObject = async (id, res) => {
-  await s3.deleteObject({ Bucket: AWS_BUCKET_NAME, Key: id }).promise();
+  await s3.deleteObject({ Bucket: AWS_S3_BUCKET_NAME, Key: id }).promise();
   res.end();
 };
 
@@ -80,4 +82,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000);
+server.listen(port);
